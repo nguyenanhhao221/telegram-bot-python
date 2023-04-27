e5f5a5ca47cba36ffd42cee829e614121e0e39c7
Hao Nguyen <hao@haonguyen.tech>
2023-04-27 17:12:38 +0700

    feat: use telegram-python-bot library, bot reply to /start command

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""
+Main entry handler for Telegram bot
+"""
 import logging
 import os
-import asyncio
-import telegram
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 from dotenv import load_dotenv
 
@@ -15,6 +18,8 @@ BOT_USERNAME = "python_vaccine_bot"
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+
+
 if not BOT_TELEGRAM_TOKEN:
     raise ValueError(
         """ BOT_TELEGRAM_TOKEN is not set correctly in environment variables. 
@@ -23,11 +28,21 @@ if not BOT_TELEGRAM_TOKEN:
     )
 
 
-async def main():
-    bot = telegram.Bot(BOT_TELEGRAM_TOKEN)
-    async with bot:
-        print(await bot.get_me())
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.effective_chat:
+        return
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Hello I'm a bot created by Hao Nguyen"
+    )
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # Start the bot
+    application = ApplicationBuilder().token(BOT_TELEGRAM_TOKEN).build()
+
+    # Add a handler when the bot receive the command "/start", and a callback function to run after it got /start
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    # This function will keep running and get update to telegram to fetch the latest update to our bot
+    application.run_polling()
