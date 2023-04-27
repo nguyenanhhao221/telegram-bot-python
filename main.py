07bf51fe67ddfdf2e2a2d8d28224705124acccb0
Hao Nguyen <hao@haonguyen.tech>
2023-04-27 17:23:22 +0700

    docs: add doc for start handler

--- main.py
+++ main.py
@@ -29,6 +29,16 @@ if not BOT_TELEGRAM_TOKEN:
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handler for the /start command. Sends a greeting message to the user.
+
+    Args:
+        update (telegram.Update): The update object representing the user's message.
+        context (telegram.ext.CallbackContext): The context object for the current update.
+
+    Returns:
+        None: The function is asynchronous and does not return anything. The message is sent using `context.bot.send_message`.
+    """
+
     if not update.effective_chat:
         return
     await context.bot.send_message(
