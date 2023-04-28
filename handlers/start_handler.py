52fef3512d0392ee498bbf9577cc9927baa4a2b2
Hao Nguyen <hao@haonguyen.tech>
2023-04-28 16:23:55 +0700

    refactor: move start handler to a separate module

---
+++ handlers/start_handler.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import (
+    ContextTypes,
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handler for the /start command. Sends a greeting message to the user.
+
+    Args:
+        update (telegram.Update): The update object representing the user's
+        message.
+        context (telegram.ext.CallbackContext): The context object for
+        the current update.
+
+    Returns:
+        None: The function is asynchronous and does not return anything.
+        The message is sent using `context.bot.send_message`.
+    """
+
+    if not update.effective_chat:
+        return
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="""Hello I'm a bot created by Hao Nguyen""",
+    )
