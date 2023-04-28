21075f561864236daaf4bd3b52e0999a4af39ce8
Hao Nguyen <hao@haonguyen.tech>
2023-04-28 16:03:28 +0700

    feat: add echo handler to bot
    - the bot will echo every message it receive provided that it a text,
      and also not a command
    - privacy mode need to be turn off in the bot settings so that the
      update got sent to our code

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ Main entry handler for Telegram bot
 import logging
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
 
 from dotenv import load_dotenv
 
@@ -46,13 +52,36 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Echoes the message sent by the user.
+
+    If the message contains no text, or if the chat is not valid, no action is taken.
+
+    Args:
+        update (telegram.Update): The update object containing information about the message.
+        context (telegram.ext.CallbackContext): The context object for the bot.
+
+    Returns:
+        None
+    """
+
+    if not update.effective_chat or not update.message or not update.message.text:
+        return
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
+
+
 if __name__ == "__main__":
     # Start the bot
     application = ApplicationBuilder().token(BOT_TELEGRAM_TOKEN).build()
 
     # Add a handler when the bot receive the command "/start", and a callback function to run after it got /start
     start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+    # Add these handler into the bot
+    application.add_handlers([start_handler, echo_handler])
 
     # This function will keep running and get update to telegram to fetch the latest update to our bot
     application.run_polling()
