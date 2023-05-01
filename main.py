d663cb794898349f270e12a86a5eba4b4ce1b537
Hao Nguyen <hao@haonguyen.tech>
2023-05-01 17:32:11 +0700

    feat: add caps handler to send back message with all capp

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from telegram.ext import (
 )
 
 from dotenv import load_dotenv
+from handlers.cap_handler import caps
 from handlers.echo_handler import echo
 from handlers.start_handler import start
 
@@ -43,10 +44,18 @@ if __name__ == "__main__":
     # Add a handler when the bot receive the command "/start",
     # and a callback function to run after it got /start
     start_handler = CommandHandler("start", start)
+
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
 
+    cap_handler = CommandHandler("caps", caps)
     # Add these handler into the bot
-    application.add_handlers([start_handler, echo_handler])
+    application.add_handlers(
+        [
+            start_handler,
+            echo_handler,
+            cap_handler,
+        ]
+    )
 
     # This function will keep running and get update to telegram to fetch
     # the latest update to our bot
